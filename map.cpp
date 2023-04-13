6c55c757f9564239ef6356543ce859a8626b12db
Nguyen Tuyet <125747869+DRM-T@users.noreply.github.com>
2023-04-13 10:33:26 +0700

    Add files via upload

---
+++ map.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main()
+{
+    int n, a[1000], i, max=0;
+    map<int, int> f;
+    cin>> n;
+
+    for (i=0; i<n; i++)
+    {
+        cin>> a[i];
+        f[a[i]]=i;
+    }
+
+    for (i=0; i<n; i++)
+        if (f[a[i]]-i+1>max)
+            max=f[a[i]]-i;
+    cout<< max;
+}
